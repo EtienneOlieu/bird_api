24ed989a6cf45382256c71c962e12dc8794f39f9
Horrid Entity <etienne.olieu@gmail.com>
2023-02-15 11:20:23 +0100

    Created getters for all, name, and family

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 const express = require("express")
 const app = express()
-app.use(express.json)
+app.use(express.json())
 
 const birdList = [
     {id : 1,
     name : 'crow',
     family : 'corvidae',
     species : 'corvus',
-    wingSpan : 150},
+    wingspan : 150},
     
     {id : 2,
     name : 'sparrow',
     family : 'prunelidae',
     species : 'prunella modularis',
-    wingSpan : 21},
+    wingspan : 21},
     
     {id : 3,
     name : 'golden eagle',
     family : 'accipitridae',
     species : 'aquila chrysaetos',
-    wingSpan : 228.6},    
+    wingspan : 228.6},    
     
     {id : 4,
     name : 'albatros',
     family : 'diomedeidae',
     species : 'diomedea epomophora',
-    wingSpan : 330},
+    wingspan : 330},
     
     {id : 5,
     name : 'robin',
     family : 'muscicapidae',
     species : 'erithacus rubecula',
-    wingSpan : 22},
+    wingspan : 22},
     
     {id : 6,
     name : 'blue jay',
     family : 'corvidae',
     species : 'cyanocitta cristata',
-    wingSpan : 43}
+    wingspan : 43},
+
+    {id : 7,
+    name : 'great tit',
+    family : 'paridae',
+    species : 'parus major',
+    wingspan : 25.5}
 ]
 app.get('/', (rew, res) => {
     res.send({message: 'nothing here'})
@@ -47,5 +53,18 @@ app.get('/birds', (req, res) => {
     res.send({listOfBirds : birdList})
     console.log(birdList)
 })
+
+app.get('/birds/:name', (req, res) => {
+    birdName = req.params.name
+    let chosenBird = birdList.find((bird) => bird.name === birdName)
+    res.send({listOfBirds : chosenBird})
+})
+
+app.get('/birds/family/:family', (req, res) => {
+    birdFamily = req.params.family
+    let familyBirds = birdList.filter(bird => bird.family === birdFamily)
+    res.send({listOfBirds : familyBirds})
+})
+
 
 app.listen(8080)
