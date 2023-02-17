a18a7f51032569ab2121e858bcd20d00b9006489
Horrid Entity <etienne.olieu@gmail.com>
2023-02-17 15:06:59 +0100

    used eslinter to clean up code

--- app.js
+++ app.js
@@ -1,79 +1,96 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
+const express = require('express');
+
+const app = express();
+app.use(express.json());
 
 const birdList = [
-    {id : 1,
-    name : 'crow',
-    family : 'corvidae',
-    species : 'corvus',
-    wingspan : 150},
-    
-    {id : 2,
-    name : 'sparrow',
-    family : 'prunelidae',
-    species : 'prunella modularis',
-    wingspan : 21},
-    
-    {id : 3,
-    name : 'golden eagle',
-    family : 'accipitridae',
-    species : 'aquila chrysaetos',
-    wingspan : 228.6},    
-    
-    {id : 4,
-    name : 'albatros',
-    family : 'diomedeidae',
-    species : 'diomedea epomophora',
-    wingspan : 330},
-    
-    {id : 5,
-    name : 'robin',
-    family : 'muscicapidae',
-    species : 'erithacus rubecula',
-    wingspan : 22},
-    
-    {id : 6,
-    name : 'blue jay',
-    family : 'corvidae',
-    species : 'cyanocitta cristata',
-    wingspan : 43},
-
-    {id : 7,
-    name : 'great tit',
-    family : 'paridae',
-    species : 'parus major',
-    wingspan : 25.5}
-]
+  {
+    id: 1,
+    name: 'crow',
+    family: 'corvidae',
+    species: 'corvus',
+    wingspan: 150,
+  },
+  {
+    id: 2,
+    name: 'sparrow',
+    family: 'prunelidae',
+    species: 'prunella modularis',
+    wingspan: 21,
+  },
+  {
+    id: 3,
+    name: 'golden eagle',
+    family: 'accipitridae',
+    species: 'aquila chrysaetos',
+    wingspan: 228.6,
+  },
+  {
+    id: 4,
+    name: 'albatros',
+    family: 'diomedeidae',
+    species: 'diomedea epomophora',
+    wingspan: 330,
+  },
+  {
+    id: 5,
+    name: 'robin',
+    family: 'muscicapidae',
+    species: 'erithacus rubecula',
+    wingspan: 22,
+  },
+  {
+    id: 6,
+    name: 'blue jay',
+    family: 'corvidae',
+    species: 'cyanocitta cristata',
+    wingspan: 43,
+  },
+  {
+    id: 7,
+    name: 'great tit',
+    family: 'paridae',
+    species: 'parus major',
+    wingspan: 25.5,
+  },
+];
 
 app.get('/', (req, res) => {
-    res.send({message: 'nothing here'})
-})
+  res.send({ message: 'nothing here' });
+});
 
 app.get('/birds', (req, res) => {
-    res.send({listOfBirds : birdList})
-    console.log(birdList)
-})
+  res.send({ data: birdList });
+});
 
-//Anders gennemgang af lektien
-app.get('birds/:id', (req, res ) => {
-    const foundBird = birdList.find(bird => bird.id === Number(req.params.id))
-    res.send({data:foundBird})
-})
+//  Anders gennemgang af lektien
+app.get('/birds/:id', (req, res) => {
+  const foundBird = birdList.find((bird) => bird.id === Number(req.params.id));
+  res.send({ data: foundBird });
+});
 
-app.get('/birds/:name', (req, res) => {
-    birdName = req.params.name
-    let chosenBird = birdList.find((bird) => bird.name === birdName)
-    res.send({listOfBirds : chosenBird})
-})
+app.get('/birds/name/:name', (req, res) => {
+  const chosenBird = birdList.find((bird) => bird.name === req.params.name);
+  res.send({ data: chosenBird });
+});
 
 app.get('/birds/family/:family', (req, res) => {
-    birdFamily = req.params.family
-    let familyBirds = birdList.filter(bird => bird.family === birdFamily)
-    res.send({listOfBirds : familyBirds})
-})
+  const familyBirds = birdList.filter((bird) => bird.family === req.params.family);
+  res.send({ data: familyBirds });
+});
 
+app.post('/birds/create', (req, res) => {
 
-app.listen(8080, () => {
-    console.log('Server is running on port', 8080)
-})
+});
+app.patch('/birds/edit/:id', (req, res) => {
+
+});
+
+app.delete('/birds/delete/:id', (req, res) => {
+
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
